e8259cfd0cbbc53805855bfdbed1a51e24df7c93
Pavan Porendla <porendlapavan06744@gmail.com>
2023-08-16 00:42:40 +0530

    job update

--- src/components/Jobsshow.js
+++ src/components/Jobsshow.js
@@ -4,25 +4,25 @@ import JobCard from "./JobCard";
 const JobsSlider = () => {
   return (
     <div data-aos="fade-left" data-aos-duration="1200" className="flex m-auto   w-auto">
-      <div className="flex overflow-x-scroll no-scrollbar">
+      <div className="flex">
         <div className="flex space-x-5  p-6">
           <JobCard
             role="Senior Full-Stack engineering"
             position="Full time position"
             location="Berlin or remote"
-            salary="5lpa"
+            salary="€65-85k, 0.5-1.50% equity share options"
           />
           <JobCard
             role="Senior Developer"
             position="Full time position"
             location="Berlin or remote"
-            salary="5lpa"
+            salary="€65-85k, 0.5-1.50% equity share options"
           />
           <JobCard
             role="Super star Intern"
             position="Full time position"
             location="Berlin or remote"
-            salary="5lpa"
+            salary="€65-85k, 0.5-1.50% equity share options"
           />
         </div>
       </div>
